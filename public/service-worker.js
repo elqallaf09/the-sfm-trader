d879ea4779166add0d86bd484033f0f069671031
elqallaf09 <elqallaf09@gmail.com>
2026-06-22 05:08:42 +0300

    Refine thesfm trader logo design

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "the-sfm-trader-ios-shell-v20260622-market-icons-1";
+const CACHE_NAME = "the-sfm-trader-ios-shell-v20260622-logo-design-1";
 const STATIC_ASSETS = [
   "/",
   "/index.html",
   "/detail.html",
-  "/styles.css?v=20260622-market-icons-1",
-  "/app.js?v=20260622-market-icons-1",
+  "/styles.css?v=20260622-logo-design-1",
+  "/app.js?v=20260622-logo-design-1",
   "/detail.js?v=20260621-detail-arabic-1",
   "/manifest.webmanifest",
   "/assets/sfm-trader-logo.svg",
